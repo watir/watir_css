65d555e8daca38551d7e37717433abdb54ac62c7
Titus Fortner <titusfortner@gmail.com>
2017-09-07 11:35:52 -0700

    add guards for watirspec testing

--- spec/watirspec_helper.rb
+++ spec/watirspec_helper.rb
@@ -8,6 +8,11 @@ WatirSpec.implementation do |watirspec|
   watirspec.name = :watir_css
   watirspec.browser_class = Watir::Browser
   watirspec.browser_args = [:chrome, opts]
+
+  watirspec.guard_proc = lambda do |watirspec_guards|
+    watir_css_guards = %i[chrome watir_css relaxed_locate]
+    watirspec_guards.any? { |guard| watir_css_guards.include?(guard) }
+  end
 end
 
 WatirSpec.run!
